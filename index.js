8337d1b2d98ae95bcb3fefeecd9deffa8480840f
JeffKatzy <JeffreyEricKatz@gmail.com>
2017-09-12 19:36:27 -0400

    adding in solution

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+// Write your code in this file!
 
+function distanceFromHqInBlocks(blockNumber){
+  if(blockNumber > 42){
+      return blockNumber - 42
+  } else {
+      return 42 - blockNumber
+  }
+}
+
+function distanceFromHqInFeet(blockNumber){
+  return distanceFromHqInBlocks(blockNumber)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+  if(start < destination){
+    return (destination - start)*264
+  } else {
+    return (start - destination)*264
+  }
+}
+
+function calculatesFarePrice(start, destination){
+  let distance = distanceTravelledInFeet(start, destination)
+  if(distance <= 400){
+    return 0
+  } else if (distance > 400 && distance <= 2000){
+    return .02*distance
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}

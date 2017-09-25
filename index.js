9cf06481c22d03fe5e8d969bcc4bc629d858de34
Gabe Jackson <gj@mail.co.de>
2017-09-25 17:21:15 -0400

    Update code style

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(blockNumber){
-  if(blockNumber > 42){
-      return blockNumber - 42
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
   } else {
-      return 42 - blockNumber
+    return 42 - blockNumber;
   }
 }
 
-function distanceFromHqInFeet(blockNumber){
-  return distanceFromHqInBlocks(blockNumber)*264
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function distanceTravelledInFeet(start, destination){
-  if(start < destination){
-    return (destination - start)*264
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
   } else {
-    return (start - destination)*264
+    return (start - destination) * 264;
   }
 }
 
-function calculatesFarePrice(start, destination){
-  let distance = distanceTravelledInFeet(start, destination)
-  if(distance <= 400){
-    return 0
-  } else if (distance > 400 && distance <= 2000){
-    return .02*distance
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
   } else if (distance > 2000 && distance < 2500) {
-    return 25
+    return 25;
   } else {
-    return 'cannot travel that far'
+    return 'cannot travel that far';
   }
 }

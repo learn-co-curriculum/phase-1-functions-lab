6931dbc06c53c11f72b920296304a5728182614b
Steven G. Harms <sgharms+github-notifications@stevengharms.com>
2018-07-03 13:43:35 -0400

    calculatesFarePrice: should reduce the base free
    Was not passing test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function calculatesFarePrice (start, destination) {
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return .02 * distance;
+    return .02 * (distance - 400);
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {

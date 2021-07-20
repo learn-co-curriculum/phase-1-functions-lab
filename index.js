f7199f862605fa36ef2216d0a0f70ef6945300f0
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-20 08:17:03 -0600

    commit tree 0bcd9306795858f83e9e3be9a97dc660a1dcdc84

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num){
+    return Math.abs(42-num);
+}
+
+function distanceFromHqInFeet(num){
+    return Math.abs((42-num)*264);
+}
+
+function distanceTravelledInFeet(num1, num2){
+    return Math.abs((num1-num2)*264);
+}
+
+function calculatesFarePrice(start, destination){
+    let totalFeet = Math.abs(destination - start) * 264;
+    if(Math.abs(destination - start) == 1 ){
+        return 0;
+    }
+    else if( totalFeet > 400 && totalFeet < 2000){
+        return (totalFeet - 400) * 0.02;
+    }
+    else if(totalFeet > 2000 && totalFeet < 2500){
+        return 25;
+    }
+    else if (totalFeet > 2500){
+        return 'cannot travel that far';
+    }
+}

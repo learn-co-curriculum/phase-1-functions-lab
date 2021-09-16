094fdd51c193341869e0acd02c715f94a9acafd6
urgensonamlama <urgensonamlama@gmail.com>
2021-09-16 15:30:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+let setpoint=42;
+let result;
+function distanceFromHqInBlocks(block=43,block2=50,block3=34){
+if(result<setpoint){
+        result= setpoint-block3;
+        return result;
+        }
+  
+ else if  (result= block-setpoint){
+        return result;
+  }
+  else if(result= block2-setpoint){
+      return result;
+  }
+ 
+}
+
+

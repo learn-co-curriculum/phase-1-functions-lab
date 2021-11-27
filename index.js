b11d985805ef0e36c2a2f408587b298d892c097c
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2021-11-27 23:58:13 +0100

    commit tree 8b0832bc92e1430fbbb1a6aa352118c398d01da4

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+const numOfBlocks = Math.abs(pickup - 42)
+return numOfBlocks
+}
+ 
+
+
+function distanceFromHqInFeet(feet){
+  const numOfBlocks = distanceFromHqInBlocks(feet)
+  const distanceInFeet = numOfBlocks * 264
+ return distanceInFeet
+}
+
+function distanceTravelledInFeet(start, destination){
+    
+    const numOfFeet = (start - destination) * 264
+    return Math.abs(numOfFeet)
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination)
+
+    if (feet <= 400) {
+        return 0;
+    } else if (feet > 400 && feet <=2000) {
+        return (feet - 400) * .02;
+    } else if (feet > 2500) {
+        return 'cannot travel that far';
+    } else if (feet > 2000) {
+        return 25;
+    }
+}

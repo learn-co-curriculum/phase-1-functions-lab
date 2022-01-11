b8ef80f891349d307594bc5d3574b715ebf44054
Eric Einerson <eaeiner.gmail.co>
2022-01-11 15:29:24 -0700

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks>42){
+        return (blocks-42)
+    }
+    else {
+        return (42-blocks)
+    }
+}

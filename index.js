4eca3fa2eb9f9c66b4ab1c96df3c2b412f7c08c6
omarabbasi <omarkabbasi@outlook.com>
2022-02-03 18:29:17 -0800

    commit tree 49a019ca6ad7e0310f1f6b7f10a333211fa59062

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   if (someValue > 42) {
+     return someValue - 42
+   } else {
+     return 42 - someValue
+   }
+  }
+
+  function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+  }
+  
+  function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264;
+    } else {
+      return (start - destination) * 264;
+    }
+  }
+  
+  function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }

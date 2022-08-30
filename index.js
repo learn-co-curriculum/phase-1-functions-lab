db47d2c3398d3e179a5ee7d15cba037d07a5b9aa
Anna Schmidt <marshall1697@gmail.com>
2022-08-30 15:29:59 -0600

    first 2 functions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(int){
+    //start block 42
+    return Math.abs(42 - int);
+};
+
+function distanceFromHqInFeet(int){
+    return (Math.abs(42-int)*264);
+};
